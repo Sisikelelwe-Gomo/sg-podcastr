24c1cfe83a0ff74d7a61a57f2d4051de0a4f246c
Sisikelelwe Gomo <jimmycabtraps@gmail.com>
2025-06-23 01:32:39 +0200

    auth

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
+// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
+  runtime: 'nodejs', // 👈 Force Node.js runtime
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
